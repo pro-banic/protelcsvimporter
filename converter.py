1f41ebc452c930d2d11cba707fb4f39be3821b65
berge <berge@prohotel-edv.de>
2020-01-05 00:09:22 +0100

    einfügen von Füllnullen erfolgreich (Bsp: cloudref auf pos 2)

--- converter.py
+++ converter.py
@@ -6,16 +6,18 @@ with open("testdaten/testdatensatz.csv", "r") as csv_file:
 
     with open("testdaten/testdatenexport.csv", "w") as export_file:
         csv_writer = csv.writer(export_file, delimiter=",")
+
         i=0
         for line in csv_reader:
             writer = csv.writer
             line_protelparameters=["uhu", "aha", "oho"]
 # todo iterieren der Kunden
-            i = i+1
+            i=i+1
             anzahl=i
             Line_iteration_kundennummer=[anzahl]
+            Line_iteration_zero=["0"]
 
-            line_new = Line_iteration_kundennummer + line + line_protelparameters
+            line_new = Line_iteration_kundennummer + Line_iteration_zero + line + line_protelparameters
             csv_writer.writerow(line_new)
 
 
