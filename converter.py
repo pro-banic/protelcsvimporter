ec82e4d08384d3e157bb8313ee2e97debb4c9470
berge <berge@prohotel-edv.de>
2020-01-04 23:55:16 +0100

    iterieren der Kundennummer erfolgreich

--- converter.py
+++ converter.py
@@ -1,19 +1,23 @@
 import csv
 
-# todo keine modifizierung der Zeilen möglich - , oder Nullen dranhängen um ins format von protel zu kommen
+
 with open("testdaten/testdatensatz.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
 
     with open("testdaten/testdatenexport.csv", "w") as export_file:
         csv_writer = csv.writer(export_file, delimiter=",")
-
+        i=0
         for line in csv_reader:
             writer = csv.writer
-
             line_protelparameters=["uhu", "aha", "oho"]
+# todo iterieren der Kunden
+            i = i+1
+            anzahl=i
+            Line_iteration_kundennummer=[anzahl]
 
-            line_new = line + line_protelparameters
+            line_new = Line_iteration_kundennummer + line + line_protelparameters
             csv_writer.writerow(line_new)
 
 
-
+            print(line_new)
+            #print(Line_iteration_kundennummer)
