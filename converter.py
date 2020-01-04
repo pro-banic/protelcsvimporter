d6711bc671d3b49835f2a61d4774073bc48d4db3
berge <berge@prohotel-edv.de>
2020-01-04 22:02:33 +0100

    schreibt csv datei mit output

--- converter.py
+++ converter.py
@@ -1,14 +1,16 @@
 import csv
 
+# todo keine modifizierung der Zeilen möglich - , oder Nullen dranhängen um ins format von protel zu kommen
 with open("testdaten/testdatensatz.csv", "r") as csv_file:
     csv_reader = csv.reader(csv_file)
 
     with open("testdaten/testdatenexport.csv", "w") as export_file:
-        csv_writer = csv.writer(export_file)
+        csv_writer = csv.writer(export_file, delimiter=",")
 
         for line in csv_reader:
             writer = csv.writer
-            csv_writer.writerow(line)
+            line_new = line
+            csv_writer.writerow(line_new)
 
 
 
