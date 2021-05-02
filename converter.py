37ab2f822c0991b69700dcbfb19ca704271c6d3a
Sebastian Berge <bergesebastian@gmail.com>
2021-05-02 13:20:34 +0200

    Import mit Mails und Telefonnummer erfolgreich

--- converter.py
+++ converter.py
@@ -138,9 +138,10 @@ def convertlvl1pairtospe():
 
 
 def csvtolvl1():
-    # TODO Fax Nummer noch hinterlegen und importierbar machen
+    # TODO Fax Nummer noch hinterlegen und importierbar machen, mit abfragen in Importtabelle
 
-    with open("testdaten/LVL1-Import-Gästedaten.csv", "r", encoding='utf8', errors='ignore') as csv_file:
+    # with open("testdaten/sigl-guests-correct-order.csv", "r", encoding='utf8', errors='ignore') as csv_file:
+    with open("testdaten/sigl-guests-correct-order.csv", "r", encoding='cp1252', errors='ignore') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=';')
 
         # Test wie importdatei eingelesen wird:
@@ -151,7 +152,7 @@ def csvtolvl1():
         # csv.reader(csvfile, dialect='excel', **fmtparams)¶
         # excel dateien direkt einlesen?
 
-        with open("testdaten/testdatenexport-lvl1.txt", "w", encoding='cp1252') as export_file:
+        with open("testdaten/sigls-datenexport-lvl2.txt", "w", encoding='cp1252') as export_file:
             # header schreiben
             header = "create table kunden (\"kdnr\" int not null default 0 ,\"cloudref\" bigint not null default 0 ,\"mpehotel\" int not null default 0 ,\"hotelkdnr\" int not null default 0 ,\"extref\" varchar(15) not null default '' ,\"member\" varchar(50) not null default '' ,\"outlsync\" int not null default 0 ,\"outldate\" datetime not null default '1900-01-01' ,\"passwd\" varchar(15) not null default '' ,\"typ\" int not null default 0 ,\"name1\" varchar(80) not null default '' ,\"name2\" varchar(80) not null default '' ,\"vorname\" varchar(50) not null default '' ,\"ehepartner\" varchar(80) not null default '' ,\"ehegeb\" datetime not null default '1900-01-01' ,\"strasse\" varchar(80) not null default '' ,\"strasse2\" varchar(80) not null default '' ,\"strasse3\" varchar(80) not null default '' ,\"plz\" varchar(17) not null default '' ,\"pfplz\" varchar(17) not null default '' ,\"postfach\" varchar(17) not null default '' ,\"ort\" varchar(50) not null default '' ,\"land\" varchar(80) not null default '' ,\"landkz\" int not null default 0 ,\"regionkz\" int not null default 0 ,\"gender\" int not null default 0 ,\"abteil\" varchar(80) not null default '' ,\"region\" varchar(80) not null default '' ,\"nat\" int not null default 0 ,\"marketing\" int not null default 0 ,\"sprache\" int not null default 0 ,\"vip\" int not null default 0 ,\"gebdat\" datetime not null default '1900-01-01' ,\"anrede\" varchar(40) not null default '' ,\"begr\" varchar(70) not null default '' ,\"telefonnr\" varchar(50) not null default '' ,\"funktel\" varchar(50) not null default '' ,\"email\" varchar(75) not null default '' ,\"twitter\" varchar(80) not null default '' ,\"homepage\" varchar(80) not null default '' ,\"cctypn\" int not null default 0 ,\"ccexp\" varchar(10) not null default '' ,\"cc\" varchar(35) not null default '' ,\"kfz\" varchar(30) not null default '' ,\"faxnr\" varchar(50) not null default '' ,\"telex\" varchar(50) not null default '' ,\"gdsident\" varchar(64) not null default '' ,\"resname\" varchar(80) not null default '' ,\"iata\" varchar(9) not null default '' ,\"contract\" varchar(20) not null default '' ,\"resvorn\" varchar(50) not null default '' ,\"resanr\" varchar(40) not null default '' ,\"restel\" varchar(100) not null default '' ,\"respanr\" varchar(70) not null default '' ,\"rechname\" varchar(80) not null default '' ,\"rechvorn\" varchar(50) not null default '' ,\"rechanr\" varchar(40) not null default '' ,\"rechtel\" varchar(100) not null default '' ,\"rechpanr\" varchar(70) not null default '' ,\"titel\" varchar(20) not null default '' ,\"firmenname\" varchar(50) not null default '' ,\"firmennam2\" varchar(50) not null default '' ,\"beruf\" varchar(50) not null default '' ,\"prof\" int not null default 0 ,\"wunschzi\" int not null default 0 ,\"zimmerattr\" varchar(50) not null default '' ,\"gfeat\" varchar(50) not null default '' ,\"preiscode\" int not null default 0 ,\"comcode\" int not null default 0 ,\"comtax1\" int not null default 0 ,\"comtax2\" int not null default 0 ,\"deleted\" int not null default 0 ,\"kredit\" int not null default 0 ,\"kreditbet\" decimal(19,2) not null default 0 ,\"intcredit\" int not null default 0 ,\"intcredita\" decimal(19,2) not null default 0 ,\"statement\" int not null default 0 ,\"sonderp\" int not null default 0 ,\"sonderp1\" decimal(7,2) not null default 0 ,\"sonderp2\" decimal(7,2) not null default 0 ,\"sonderp3\" decimal(7,2) not null default 0 ,\"sonderp4\" decimal(7,2) not null default 0 ,\"bemerkung\" varchar(250) not null default '' ,\"bemrest\" varchar(250) not null default '' ,\"aufenth\" int not null default 0 ,\"naechte\" int not null default 0 ,\"noshows\" int not null default 0 ,\"stornos\" int not null default 0 ,\"aufenth_vj\" int not null default 0 ,\"naechte_vj\" int not null default 0 ,\"noshows_vj\" int not null default 0 ,\"stornos_vj\" int not null default 0 ,\"letzterauf\" datetime not null default '1900-01-01' ,\"firststay\" datetime not null default '1900-01-01' ,\"erfasst\" datetime not null default '1900-01-01' ,\"erfassttim\" varchar(10) not null default '' ,\"erfasstusr\" varchar(50) not null default '' ,\"letzterpr\" decimal(19,2) not null default 0 ,\"letzteszi\" varchar(10) not null default '' ,\"fibudeb\" varchar(20) not null default '' ,\"logis\" decimal(19,2) not null default 0 ,\"fb\" decimal(19,2) not null default 0 ,\"extras\" decimal(19,2) not null default 0 ,\"logis_vj\" decimal(19,2) not null default 0 ,\"fb_vj\" decimal(19,2) not null default 0 ,\"extras_vj\" decimal(19,2) not null default 0 ,\"transfer\" int not null default 0 ,\"mailing\" int not null default 0 ,\"protected\" int not null default 0 ,\"changed\" datetime not null default '1900-01-01' ,\"changedby\" varchar(50) not null default '' ,\"changetime\" varchar(10) not null default '' ,\"merged\" datetime not null default '1900-01-01' ,\"mergetime\" varchar(10) not null default '' ,\"tosales\" int not null default 0 ,\"passnr\" varchar(30) not null default '' ,\"issued\" varchar(50) not null default '' ,\"issuedate\" datetime not null default '1900-01-01' ,\"discount\" int not null default 0 ,\"doctype\" int not null default 0 ,\"docvalid\" datetime not null default '1900-01-01' ,\"vatno\" varchar(30) not null default '' ,\"flags\" varchar(20) not null default '' ,\"afmno\" varchar(30) not null default '' ,\"gebort\" varchar(50) not null default '' ,\"gebland\" int not null default 0 ,\"mstatus\" int not null default 0 ,\"mahncode\" int not null default 0 ,\"masteracc\" int not null default 0 ,\"masterdeb\" int not null default 0 ,\"ismstdeb\" int not null default 0 ,\"extend\" int not null default 0 ,\"spfrom1\" datetime not null default '1900-01-01' ,\"spfrom2\" datetime not null default '1900-01-01' ,\"spfrom3\" datetime not null default '1900-01-01' ,\"spfrom4\" datetime not null default '1900-01-01' ,\"spto1\" datetime not null default '1900-01-01' ,\"spto2\" datetime not null default '1900-01-01' ,\"spto3\" datetime not null default '1900-01-01' ,\"spto4\" datetime not null default '1900-01-01' ,\"sp1p1\" decimal(7,2) not null default 0 ,\"sp2p1\" decimal(7,2) not null default 0 ,\"sp3p1\" decimal(7,2) not null default 0 ,\"sp4p1\" decimal(7,2) not null default 0 ,\"sp1p2\" decimal(7,2) not null default 0 ,\"sp2p2\" decimal(7,2) not null default 0 ,\"sp3p2\" decimal(7,2) not null default 0 ,\"sp4p2\" decimal(7,2) not null default 0 ,\"sp1p3\" decimal(7,2) not null default 0 ,\"sp2p3\" decimal(7,2) not null default 0 ,\"sp3p3\" decimal(7,2) not null default 0 ,\"sp4p3\" decimal(7,2) not null default 0 ,\"sp1p4\" decimal(7,2) not null default 0 ,\"sp2p4\" decimal(7,2) not null default 0 ,\"sp3p4\" decimal(7,2) not null default 0 ,\"sp4p4\" decimal(7,2) not null default 0 ,\"user00\" varchar(20) not null default '' ,\"user01\" varchar(20) not null default '' ,\"user02\" int not null default 0 ,\"user03\" int not null default 0 ,\"taxexemp\" int not null default 0 ,\"voidfee\" int not null default 0 ,\"depcode\" int not null default 0 ,\"laundry\" int not null default 0 ,\"posrcd\" int not null default 0 ,\"emptybfee\" int not null default 0 ,\"shortname\" varchar(80) not null default '' ,\"mastername\" varchar(80) not null default '' ,\"accountname\" varchar(80) not null default '' ,\"otapara\" varchar(51) not null default '' ,\"numcontacts\" int not null default 0 ,\"numaccounts\" int not null default 0 ,\"addcleansed\" int not null default 0 ,\"cpssrc\" varchar(30) not null default '' ,\"cpsid\" varchar(30) not null default '' ,\"_del\" int not null default 0 )\n"
             export_file.write(header)
@@ -171,6 +172,7 @@ def csvtolvl1():
                 Line_mpe = ["1"]
                 Line_emptyvalue = [""]
                 Line_emptydate = ["1900 - 01 - 01"]
+                # TODO   0 = privat | 1 = Firma mit abfragen in Importtabelle bei Karteityp
                 Line_Karteityp = ["0"]
                 Line_Nachname = [line[0]]
                 Line_Vorname = [line[1]]
@@ -210,16 +212,22 @@ def csvtolvl1():
 
                 csv_writer.writerow(line_new)
 
-                print(line_new)
+                # print(line_new)
 
                # Gastkommunikationsarten - gcom table - Content creation
                 j += 1
                 if Line_Mail != "":
                     #gcomline = [j, Line_iteration_kundennummer, 1, Line_Mail]
                     gcomline = j, anzahl, 1, line[8], "", 0, 0
+                    # print(line[8])
                     # 1,838,1,'test@test.de','',0,0
                     # i,Kundennummer,Kommunikationsart,Inhalt,'',0,0
-                    inhaltssammler_gcom.append(gcomline)
+                    inhaltssammler_gcom.append(str(gcomline)[1:-1])
+                if Line_Tel != "":
+                    gcomline = j, anzahl, 2, line[9], "", 0, 0
+                    # print(line[9])
+                    inhaltssammler_gcom.append(str(gcomline)[1:-1])
+                    # print(str(gcomline)[1:-1])
 
             # header natcode schreiben
             header_natcode = "create table natcode (\"abkuerz\" varchar(20) not null default '' ,\"land\" varchar(80) not null default '' ,\"statnr\" int not null default 0 ,\"codenr\" int not null default 0 ,\"sort\" int not null default 0 ,\"gruppe\" int not null default 0 ,\"brkopftyp\" int not null default 0 ,\"sprache\" int not null default 0 ,\"isocode\" varchar(4) not null default '' ,\"state\" varchar(80) not null default '' ,\"showfo\" int not null default 0 ,\"inet\" int not null default 0 ,\"nation\" varchar(80) not null default '' ,\"addinfo\" varchar(50) not null default '' ,\"user01\" int not null default 0 ,\"anreisen1\" int not null default 0 ,\"anzueber1\" int not null default 0 ,\"anreisen2\" int not null default 0 ,\"anzueber2\" int not null default 0 ,\"anreisen3\" int not null default 0 ,\"anzueber3\" int not null default 0 ,\"anreisen4\" int not null default 0 ,\"anzueber4\" int not null default 0 ,\"anreisen5\" int not null default 0 ,\"anzueber5\" int not null default 0 ,\"anreisen6\" int not null default 0 ,\"anzueber6\" int not null default 0 ,\"anreisen7\" int not null default 0 ,\"anzueber7\" int not null default 0 ,\"anreisen8\" int not null default 0 ,\"anzueber8\" int not null default 0 ,\"anreisen9\" int not null default 0 ,\"anzueber9\" int not null default 0 ,\"anreisen10\" int not null default 0 ,\"anzueber10\" int not null default 0 ,\"anreisen11\" int not null default 0 ,\"anzueber11\" int not null default 0 ,\"anreisen12\" int not null default 0 ,\"anzueber12\" int not null default 0 )\n"
@@ -233,9 +241,9 @@ def csvtolvl1():
             export_file.write(header_gcom)
 
             # Gastkommunikationsarten - gcom table - content deploy
-            print("updated Inhaltssammler:", inhaltssammler_gcom)
+            #print("updated Inhaltssammler:", inhaltssammler_gcom)
             for line in inhaltssammler_gcom:
-                print(line)
+                # print(line)
                 export_file.write(str(line)+'\n')
 
             # header gcomref table schreiben
@@ -245,7 +253,7 @@ def csvtolvl1():
             # Gastkommunikationsarten - gcomref table - content deploy
             gcomref_mpehotel = 1
             gcomref_end = i  # letzte Kundennummer
-            gcomref_content = str(gcomref_mpehotel) + ", " + str(gcomref_end)
+            gcomref_content = str(gcomref_mpehotel) + "," + str(gcomref_end)
             export_file.write(gcomref_content+'\n')
 
             # header gcomtref table schreiben - types of communication
@@ -256,7 +264,7 @@ def csvtolvl1():
             gcomtref_mpehotel = 1
             gcomtref_end = 3  # Anzahl der Kommunikationsarten, Bsp: Mail + Tele + Fax = 3
             gcomtref_content = str(gcomtref_mpehotel) + \
-                ", " + str(gcomtref_end)
+                "," + str(gcomtref_end)
             export_file.write(gcomtref_content+'\n')
 
             # header gcomtype table schreiben - types of communication
@@ -266,7 +274,6 @@ def csvtolvl1():
             # Gastkommunikationsarten - gcomtype table - content deploy - types of communication - Anzahl der Kommunikationsarten
             content_gcomtype = "1,0,'E-Mail','E-Mail','','',19,-1,-1,-1,-1,'','',0,0,0,0,0,0,0\n2,0,'Telefon','Telefon','','',18,-1,-1,-1,-1,'','',0,0,0,0,0,0,0\n3,0,'Mobil','Mobil','','',18,-1,-1,-1,-1,'','',0,0,0,0,0,0,0\n4,0,'Fax','Fax','','',18,-1,-1,-1,-1,'','',0,0,0,0,0,0,0\n"
             export_file.write(content_gcomtype)
-
 
             # header natcode > 1 = Deutschland
             # create table natcode ("abkuerz" varchar(20) not null default '' ,"land" varchar(80) not null default '' ,"statnr" int not null default 0 ,"codenr" int not null default 0 ,"sort" int not null default 0 ,"gruppe" int not null default 0 ,"brkopftyp" int not null default 0 ,"sprache" int not null default 0 ,"isocode" varchar(4) not null default '' ,"state" varchar(80) not null default '' ,"showfo" int not null default 0 ,"inet" int not null default 0 ,"nation" varchar(80) not null default '' ,"addinfo" varchar(50) not null default '' ,"user01" int not null default 0 ,"anreisen1" int not null default 0 ,"anzueber1" int not null default 0 ,"anreisen2" int not null default 0 ,"anzueber2" int not null default 0 ,"anreisen3" int not null default 0 ,"anzueber3" int not null default 0 ,"anreisen4" int not null default 0 ,"anzueber4" int not null default 0 ,"anreisen5" int not null default 0 ,"anzueber5" int not null default 0 ,"anreisen6" int not null default 0 ,"anzueber6" int not null default 0 ,"anreisen7" int not null default 0 ,"anzueber7" int not null default 0 ,"anreisen8" int not null default 0 ,"anzueber8" int not null default 0 ,"anreisen9" int not null default 0 ,"anzueber9" int not null default 0 ,"anreisen10" int not null default 0 ,"anzueber10" int not null default 0 ,"anreisen11" int not null default 0 ,"anzueber11" int not null default 0 ,"anreisen12" int not null default 0 ,"anzueber12" int not null default 0 )
@@ -311,6 +318,9 @@ def csvtolvl1():
             # 'USA','USA',71,36,0,0,2,-1,'US','',0,1,'','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
             # 'NL','Niederlande',31,13,0,0,2,-1,'NL','',0,1,'','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
             # 'AUS','Australien',75,40,0,0,0,-1,'AU','',0,1,'','',0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+            print("all done: ", i, " Gäste")
+
+
 csvtolvl1()
 
 
@@ -318,7 +328,7 @@ def addgcomtolvl1():
     '''zu Level 1 müssen noch die Mailadressen und Telefonnummer usw als GCom Tabelle hinzugefügt werden'''
 
     # vorab erstellten lvl 1 Datensatz wieder einlesen:
-    with open("testdaten/testdatenexport-lvl1.txt", "r", encoding='utf8', errors='ignore') as csv_file:
+    with open("testdaten/testdatenexport-lvl1.txt", "r", encoding='cp1252', errors='ignore') as csv_file:
         csv_reader = csv.reader(csv_file)
         for line in csv_reader:
             print("Importdatei inhalt: ", line)
